686214cbdbfe40dd3e09e7dd0137f8eefb138f31
Thomas Warren <D00157801@student.dkit.ie>
2018-04-16 20:55:58 +0100

    Controllers and bootsrap added. Barebones header set up.

---
+++ forumwebsiteCA3/Controllers/commentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace forumwebsiteCA3.Controllers
+{
+    public class commentController : Controller
+    {
+        // GET: comment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ forumwebsiteCA3/Controllers/usersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace forumwebsiteCA3.Controllers
+{
+    public class usersController : Controller
+    {
+        // GET: users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
