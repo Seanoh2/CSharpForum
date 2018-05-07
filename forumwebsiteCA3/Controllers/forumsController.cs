dbbd1c78116e0eb0978ed3b7081898893fd75d33
Thomas Warren <D00157801@student.dkit.ie>
2018-05-07 21:48:12 +0100

    added cover photos that dynamicaly changes based on the board being viewed

--- forumwebsiteCA3/Controllers/forumsController.cs
+++ forumwebsiteCA3/Controllers/forumsController.cs
@@ -5,35 +5,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace forumwebsiteCA3.Controllers
-{
-    public class forumsController : Controller
-    {
+namespace forumwebsiteCA3.Controllers {
+    public class forumsController : Controller {
         private ApplicationDbContext _context;
 
-        public forumsController()
-        {
+        public forumsController() {
             _context = new ApplicationDbContext();
         }
 
-        protected override void Dispose(bool disposing)
-        {
+        protected override void Dispose(bool disposing) {
             _context.Dispose();
         }
 
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             var forum = from f in _context.forums select f;
             return View("allBoards", forum);
         }
 
-        public ActionResult goToBoardById(int forumid)
-        {
+        public ActionResult goToBoardById(int forumid) {
 
             var post = _context.posts.Where(p => p.forumID == forumid);
 
             return View("board", post);
         }
-
     }
 }
